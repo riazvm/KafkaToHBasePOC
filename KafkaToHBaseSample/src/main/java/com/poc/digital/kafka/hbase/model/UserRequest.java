4a9757fd75c291d561e619c75fbd14ec08b34bb8
Riaz Mohamed <riazm@riazm-a01.vmware.com>
2019-04-15 23:31:33 -0500

    added user object

--- KafkaToHBaseSample/src/main/java/com/poc/digital/kafka/hbase/model/UserRequest.java
+++ KafkaToHBaseSample/src/main/java/com/poc/digital/kafka/hbase/model/UserRequest.java
@@ -13,4 +13,12 @@ public class UserRequest {
 	
 	private User user;
 
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
 }
--- KafkaToHBaseSample/src/main/java/com/poc/digital/kafka/hbase/util/kInitializer.java
+++ KafkaToHBaseSample/src/main/java/com/poc/digital/kafka/hbase/util/kInitializer.java
@@ -1,15 +1,7 @@
 package com.poc.digital.kafka.hbase.util;
 
-import javax.annotation.PostConstruct;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-//import org.springframework.boot.context.event.ContextRe;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
